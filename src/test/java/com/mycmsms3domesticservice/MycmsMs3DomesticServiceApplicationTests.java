8130feb54061bd8ed45167c22222e4c21813369c
DzakirinMD <dzakirinmd@gmail.com>
2023-04-12 10:54:57 +0800

    Added base project

---
+++ src/test/java/com/mycmsms3domesticservice/MycmsMs3DomesticServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycmsms3domesticservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycmsMs3DomesticServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
