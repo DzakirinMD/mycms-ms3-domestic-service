76d178a2fe2ef7c03338a53321bbf03b1d9e94dc
dzakirin-muhammad <muhammad.dzakirin-a.r@edag.com.my>
2023-04-18 17:22:19 +0800

    added kafka consumer to create new domestic transfer trx

--- src/main/java/com/mycmsms3domesticservice/entity/DomesticTransfer.java
+++ src/main/java/com/mycmsms3domesticservice/entity/DomesticTransfer.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -26,6 +27,7 @@ public class DomesticTransfer {
     private Long id;
 
     @NonNull
+    @Column(unique=true)
     private String domesticTransferTrxId;
 
     @NonNull
---
+++ src/main/java/com/mycmsms3domesticservice/kafka/consumer/DomesticTransferConsumer.java
@@ -0,0 +1,49 @@
+package com.mycmsms3domesticservice.kafka.consumer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mycmsms3domesticservice.entity.DomesticTransfer;
+import com.mycmsms3domesticservice.entity.dto.DomesticTransferEventDTO;
+import com.mycmsms3domesticservice.repository.DomesticTransferRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DomesticTransferConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DomesticTransferConsumer.class);
+
+    @Autowired
+    private DomesticTransferRepository domesticTransferRepository;
+
+    @KafkaListener(
+            topics = "${spring.kafka.topic.name}",
+            groupId = "${spring.kafka.consumer.group-id}"
+    )
+    public void createDomesticTransferKafka(String message) {
+        try {
+            LOGGER.info(String.format("Domestic Transfer Event received in domestictransfers-service => %s", message));
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            DomesticTransferEventDTO domesticTransferEventDTO = objectMapper.readValue(message, DomesticTransferEventDTO.class);
+
+            DomesticTransfer domesticTransfer = new DomesticTransfer();
+            domesticTransfer.setId(domesticTransferEventDTO.getDomesticTransferDTO().getId());
+            domesticTransfer.setDomesticTransferTrxId(domesticTransferEventDTO.getDomesticTransferDTO().getDomesticTransferTrxId());
+            domesticTransfer.setDomesticTransferTrxName(domesticTransferEventDTO.getDomesticTransferDTO().getDomesticTransferTrxName());
+            domesticTransfer.setDomesticTransferTrxEmailRecipient(domesticTransferEventDTO.getDomesticTransferDTO().getDomesticTransferTrxEmailRecipient());
+            domesticTransfer.setDomesticTransferTrxAmount(domesticTransferEventDTO.getDomesticTransferDTO().getDomesticTransferTrxAmount());
+
+            domesticTransferRepository.save(domesticTransfer);
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e.getMessage());
+        } catch (DataIntegrityViolationException e) {
+            throw new RuntimeException("Constraint violation occurred while saving domestic transfer: " + e.getMessage());
+        }
+    }
+}
